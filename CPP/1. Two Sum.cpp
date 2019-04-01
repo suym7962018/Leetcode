01b8d62f096706a868917109bdcdd1fe544886f3
suym7962018 <ys3587@nyu.edu>
2019-04-01 18:28:54 -0400

    Update 1. Two Sum.cpp
    hash table

--- CPP/1. Two Sum.cpp
+++ CPP/1. Two Sum.cpp
@@ -16,3 +16,18 @@ public:
     }
 };
 
+//hash table
+//O(n)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        for(int i = 0; i < nums.size(); i++){
+        	auto p = map.find(target - nums[i]);
+        	if(p != map.end())
+        		return {p -> second, i};
+        	map[nums[i]] = i;
+        }
+        return {};
+    }
+};
