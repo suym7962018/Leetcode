a156595ed9cb8583f9bfa6783080ba0d4eea71aa
suym7962018 <ys3587@nyu.edu>
2019-04-16 16:22:21 -0400

    Create 13. Roman to Integer.cpp

---
+++ CPP/13. Roman to Integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int num = 0;
+        for(int i = 0; i < s.size(); i++){
+        	if(i < (s.size() - 1) && romanCharToInt(s[i]) < romanCharToInt(s[i + 1]))
+        		num -= romanCharToInt(s[i]);
+        	else
+        		num += romanCharToInt(s[i]);
+        }
+        return num;
+    }
+
+    int romanCharToInt(char c) {
+    	switch(c) {
+    		case 'I': return 1;
+    		case 'V': return 5;
+    		case 'X': return 10;
+    		case 'L': return 50;
+    		case 'C': return 100;
+        	case 'D': return 500;
+        	case 'M': return 1000;
+        	default:  return 0;
+    	}
+    }
+};
