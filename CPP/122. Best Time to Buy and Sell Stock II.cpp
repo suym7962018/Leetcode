acd22e0140d4d651fade32dda5eb8011f3fd2e94
suym7962018 <ys3587@nyu.edu>
2019-05-02 17:27:11 -0400

    Create 122. Best Time to Buy and Sell Stock II.cpp

---
+++ CPP/122. Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,13 @@
+//to find a monotone subsequence
+//not buy and sell on same day
+//res will offset, the biggest minus smallest
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int res = 0;
+        for(int i = 1; i < prices.size(); i++){
+        	res += max(0, prices[i] - prices[i - 1]);
+        }
+        return res;
+    }
+};
